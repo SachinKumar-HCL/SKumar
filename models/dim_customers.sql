d710caf6f7819a2700e9b74f9c002d5a3d56f58c
Sachin Kumar <kumar.sachin@hcl.com>
2021-03-16 11:31:59 +0000

    Dim_Customers model updated

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,3 +1,5 @@
+
+
 {{ config (
     materialized="table"
 )}}
@@ -5,13 +7,13 @@
 
 with customers as (
 
-    select * from {{ ref('stg_customers')}}
+        select * from {{ ref('stg_customers')}}
 
 ),
 
 orders as (
 
-    select * from {{ ref('stg_orders') }}
+        select * from {{ ref('stg_orders')}}
 
 ),
 
