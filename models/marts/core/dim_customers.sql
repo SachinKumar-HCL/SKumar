f843f5a82bf32fb49c10f1eb3b65efe839ef5cc7
Sachin Kumar <kumar.sachin@hcl.com>
2021-03-16 12:57:57 +0000

    Restructure the folders

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,19 +1,13 @@
 
-
-{{ config (
-    materialized="table"
-)}}
-
-
 with customers as (
 
-        select * from {{ ref('stg_customers')}}
+    select * from {{ ref('stg_customers')}}
 
 ),
 
 orders as (
 
-        select * from {{ ref('stg_orders')}}
+    select * from {{ ref('stg_orders') }}
 
 ),
 
