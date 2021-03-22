26ff823ad8c473be41f494d7125de077ba50aead
Sachin Kumar <kumar.sachin@hcl.com>
2021-03-22 08:51:41 +0000

    commited

--- models/staging/jaffle_shop/stripe/stg_payments.sql
+++ models/staging/jaffle_shop/stripe/stg_payments.sql
@@ -1,5 +1,11 @@
-select 
-    id from customer_id,
-    first_name,
-    last_name
-from {{ source('jaffle_shop','customers') }}
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from raw.stripe.payment 
